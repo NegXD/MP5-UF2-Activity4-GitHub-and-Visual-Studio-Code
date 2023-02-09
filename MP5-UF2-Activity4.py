f02debbe60b1d8a5b7f7664407478566137ba4c1
Geng <a2208016@institutmontilivi.cat>
2023-02-09 17:01:10 +0100

    Opció 2 - Gràfic Notes

--- MP5-UF2-Activity4.py
+++ MP5-UF2-Activity4.py
@@ -5,7 +5,7 @@ clear = lambda: os.system('cls')
 def MostrarOpcions():
     print("""
 1 - Mostrar Taula de Multiplicar
-2 -
+2 - Grafic de Notes
 3 -
 4 -
 5 -
@@ -33,8 +33,60 @@ def TaulaMultiplicar():
         for i in range(1,11):
             linia = linia + str(i * a) + "\t"
         print(linia)
-    MostrarMissatge("Entra qualsevol tecla per continuar")
+    MostrarMissatge("Entra tecla enter per continuar")
 
+# Gràfic de Notes
+def GraficNotes():
+    from tkinter import Tk, Canvas, mainloop, Button
+    formatCorrecte = False
+    try:
+        print("Núm de alumnes que han tret Suspesos->", end="")
+        suspesos = int(input())
+        if (suspesos < 0 or suspesos > 1000):
+            raise Exception
+        print("Núm de alumnes que han tret Suficients ->", end="")
+        suficients = int(input())
+        if (suficients < 0 or suficients > 1000):
+            raise Exception
+        print("Núm de alumnes que han tret Notables->", end="")
+        notables = int(input())
+        if (notables < 0 or notables > 1000):
+            raise Exception
+        print("Núm de alumnes que han tret Excel·lents->", end="")
+        excellents = int(input())
+        if (excellents < 0 or excellents > 1000):
+            raise Exception
+        formatCorrecte = True  
+    except:
+        print("Error el format no és correcte, Ha de ser de 0 a 1000")
+
+    if (formatCorrecte):
+        totals = suficients + suspesos + notables + excellents
+        aP = (suficients / totals) * 360
+        sP1= (suspesos / totals) * 360
+        nP1 = (notables / totals) * 360
+        eP = (excellents / totals) * 360
+        sP = sP1 + aP
+        nP = sP + nP1
+        coord = (100,100,500,500)
+        finestra = Tk()
+        canvas = Canvas(finestra, width=700, height=600)
+        canvas.create_arc(coord,start = 0,extent = aP,fill ="yellow",width ="2")
+        canvas.create_arc(coord,start = aP,extent = sP1,fill ="blue",width ="2")
+        canvas.create_arc(coord,start = sP,extent = nP1,fill ="red",width ="2")
+        canvas.create_arc(coord,start = nP,extent = eP,fill ="green",width ="2")
+        canvas.create_rectangle(550,270,570,290,fill="blue",width="1")
+        canvas.create_text(600,280,text="Suspesos")
+        canvas.create_rectangle(550,300,570,320,fill="yellow",width="1")
+        canvas.create_text(600,310,text="Suficients")
+        canvas.create_rectangle(550,330,570,350,fill="red",width="1")
+        canvas.create_text(600,340,text="Notables")
+        canvas.create_rectangle(550,360,570,380,fill="green",width="1")
+        canvas.create_text(600,370,text="Excel·lents")
+        canvas.pack()
+        mainloop()
+    
+    MostrarMissatge("Entra tecla enter per continuar")
 
 # Main
 tecla = 10
@@ -55,7 +107,7 @@ while (tecla != 0):
         if (tecla == 1):
             TaulaMultiplicar()
         elif (tecla == 2):
-            print()
+            GraficNotes()
         elif (tecla == 3):
             print()
         elif (tecla == 4):
@@ -65,5 +117,5 @@ while (tecla != 0):
         elif (tecla == 6):
             print()
         elif (tecla == 0) :
-            MostrarMissatge("Clica Qualsevol Tecla Per Sortir Del Programa")
+            MostrarMissatge("Entra Tecla Enter Per Sortir Del Programa")
 
