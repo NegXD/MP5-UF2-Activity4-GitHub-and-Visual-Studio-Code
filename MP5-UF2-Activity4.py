c4ac27e084a19385ae78b246a7dc4b485023da5e
Geng <a2208016@institutmontilivi.cat>
2023-02-09 16:22:19 +0100

    Opció 1 - Taula de Multiplicar

--- MP5-UF2-Activity4.py
+++ MP5-UF2-Activity4.py
@@ -4,7 +4,7 @@ clear = lambda: os.system('cls')
 # Mostrar Opcions
 def MostrarOpcions():
     print("""
-1 -
+1 - Mostrar Taula de Multiplicar
 2 -
 3 -
 4 -
@@ -17,9 +17,23 @@ def MostrarOpcions():
 
 # Mostrar Missatge
 def MostrarMissatge(msg):
-    print(msg)
+    print(msg, end="")
     input()
+    clear()
 
+# Taula de Multiplicar
+def TaulaMultiplicar():
+    linia = "X \t"
+    for i in range(1, 11):
+        linia = linia + str(i) + "\t"
+    print(linia)
+
+    for a in range(1,11):
+        linia = f"{a} \t"
+        for i in range(1,11):
+            linia = linia + str(i * a) + "\t"
+        print(linia)
+    MostrarMissatge("Entra qualsevol tecla per continuar")
 
 
 # Main
@@ -39,7 +53,7 @@ while (tecla != 0):
     if (correcte):
         clear()
         if (tecla == 1):
-            print()
+            TaulaMultiplicar()
         elif (tecla == 2):
             print()
         elif (tecla == 3):
